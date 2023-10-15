18303ac7d2acc1a55da40e85e4c6dccc040bdc08
Anirban Mondal <81602017+SANU-MONDAL@users.noreply.github.com>
2023-10-15 23:40:32 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,49 @@
+<!--/footer-->
+<div class="footer">
+    <div class="footer-top">
+        <div class="col-md-4 footer-grid">
+            <h4>PLACEMENT Management System</h4>
+            <ul class="bottom">
+                <li>Designed By: Team Titan</li>
+            </ul>
+        </div>
+        <div class="col-md-4 footer-grid">
+            <h4>Message Us Now</h4>
+            <ul class="bottom">
+                <li><i class="glyphicon glyphicon-home"></i>Available 24/7 </li>
+                <li><i class="glyphicon glyphicon-envelope"></i><a href="teamtitan2022@gmail.com">teamtitan2022@gmail.com</a></li>
+                <li><i class="glyphicon glyphicon-envelope"></i>9876543210</li>
+            </ul>
+        </div>
+        <div class="col-md-4 footer-grid">
+            <h4>Address Location</h4>
+            <ul class="bottom">
+                <li><i class="glyphicon glyphicon-map-marker"></i>JIS College of Engineering.</li>
+                <li><i class="glyphicon glyphicon-earphone"></i>Mobile: 9876543210 </li>
+            </ul>
+        </div>
+        <div class="clearfix"> </div>
+    </div>
+</div>
+<div class="copy">
+    <p>&copy; 2022 Placement Management System All Rights Reserved by Team Titan</p>
+</div>
+<div class="clearfix"> </div>
+<!--//footer-->
+<!--start-smooth-scrolling-->
+<script type="text/javascript">
+    $(document).ready(function() {
+        var defaults = {
+            containerID: 'toTop', // fading element id
+            containerHoverID: 'toTopHover', // fading element hover id
+            scrollSpeed: 1200,
+            easingType: 'linear'
+        };
+        $().UItoTop({ easingType: 'easeOutQuart' });
+    });
+</script>
+<a href="#home" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+
+
+</body>
+</html>
